43a1cc3019b2b95834f5935a9a0fb45ad53dc8c1
tb17kin <sena375oya@outlook.jp>
2022-06-16 19:22:39 +0900

    初期値が分かった（たぶん）

--- RobotArm.ino
+++ RobotArm.ino
@@ -8,12 +8,43 @@ Servo wrist_ver;
 Servo wrist_rot;
 Servo gripper;
 
+float m1;
+float m2;
+float m3;
+float m4;
+float m5;
+float m6;
+
 void setup() {
-  // put your setup code here, to run once:
+  // アームがまっすぐな位置(たぶん)
+  m1 = 0;
+  m2 = 102;
+  m3 = 108;
+  m4 = 83;
+  m5 = 85;
+  m6 = 30;
+  
   Braccio.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Braccio.ServoMovement(20, 90, 90, 90, 90, 90, 73);
+
+  // 第３関節(m4)の動き
+  if(m4 == 83)
+  {
+    m4 = 63;
+  }else if(m4 == 63){
+    m4 = 73;
+  }else{
+    m4 = 83;
+  }
+
+  // グリッパー(m6)の開閉
+  if(m6 == 60)
+  {
+    m6 = 30;
+  }else{
+    m6 = 60;
+  }
+  Braccio.ServoMovement(20, m1, m2, m3, m4, m5, m6);
 }
