1e1576981e7b8afd96fcc329f892c7c0a198efaa
tb17kin <sena375oya@outlook.jp>
2022-06-29 12:49:42 +0900

    シリアル通信の確立

--- RobotArm.ino
+++ RobotArm.ino
@@ -1,50 +1,73 @@
-#include <Braccio.h>
+// #include <Braccio.h>
 #include <Servo.h>
 
-Servo base;
-Servo shoulder;
-Servo elbow;
-Servo wrist_ver;
-Servo wrist_rot;
-Servo gripper;
+// Servo base;
+// Servo shoulder;
+// Servo elbow;
+// Servo wrist_ver;
+// Servo wrist_rot;
+// Servo gripper;
 
-float m1;
-float m2;
-float m3;
-float m4;
-float m5;
-float m6;
+// float m1;
+// float m2;
+// float m3;
+// float m4;
+// float m5;
+// float m6;
+
+int flag = 0;
+// int inByte = 0;
+// String str;
+int SA = 0;
+Servo servo;
+String serial_recv;
+String line;
+int line_len;
+const int LED = 13;
+const int PIN_SERVO = 10;
+int val=180;
+int PULSE_WIDTH;
+int val_before;
 
 void setup() {
   // アームがまっすぐな位置(たぶん)
-  m1 = 0;
-  m2 = 102;
-  m3 = 108;
-  m4 = 83;
-  m5 = 85;
-  m6 = 30;
+  // m1 = 0;
+  // m2 = 102;
+  // m3 = 108;
+  // m4 = 83;
+  // m5 = 85;
+  // m6 = 30;
   
-  Braccio.begin();
+  // Braccio.begin();
+
+  Serial.begin(9600);
+  
+  pinMode(LED,OUTPUT);
+  pinMode(PIN_SERVO,OUTPUT);
+  // str = Serial.readString();
+  // Serial.println(str);
+  // Serial.println("B");
 }
 
 void loop() {
+  if(Serial.available() > 0){
+      //serial_recv = Serial.read(); // シリアル通信を受信
+      line = Serial.readStringUntil(';');
+      line_len = line.length();
+      if((line_len >= 1) && (line_len <= 3)) {
+        val = line.toInt();
+        Serial.println(val);
+      }
+      }
+  if(val == 0){
+      val = val_before;
+    }
+  PULSE_WIDTH = map(val,0,180,500,2400);
+      digitalWrite(PIN_SERVO,HIGH);
+      delayMicroseconds(PULSE_WIDTH);
+      digitalWrite(PIN_SERVO,LOW);
+  val_before = val;
 
   // 第３関節(m4)の動き
-  if(m4 == 83)
-  {
-    m4 = 63;
-  }else if(m4 == 63){
-    m4 = 73;
-  }else{
-    m4 = 83;
-  }
-
-  // グリッパー(m6)の開閉
-  if(m6 == 60)
-  {
-    m6 = 30;
-  }else{
-    m6 = 60;
-  }
-  Braccio.ServoMovement(20, m1, m2, m3, m4, m5, m6);
+  // Braccio.ServoMovement(20, m1, m2, m3, m4, m5, m6);
 }
